7736d98a0de0bd214eb8e1e6c0c288bd6620c877
Bibesh Basnet <bbibesh8@gmail.com>
2024-01-01 13:55:36 +0545

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import {Route,Routes } from "react-router-dom";
 import Home from "./components/Home";   
-import Header from "./Header";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import { PopularJobs } from "./components/Jobs";
 import JobDetails from "./components/JobDetails";
+import Header from "./components/Header";
 
 
 function App() {
