aca4b00451f90d970de47e951bb0cdbfef433ed9
Bibesh Basnet <bbibesh8@gmail.com>
2024-02-01 00:09:30 +0545

    gg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -69,24 +69,148 @@ export default function Home() {
 
 
 
-export function Banner(props){  
+export function PopularJobs() {
+  const [getJobs, setJobs] = useState([]);
+  const [rerenderJobs, setRerenderJobs] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const userDetails = useSelector((state) => state.user.value);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/getJob")
+      .then((res) => {
+        console.log("popularjobsasdasd", res.data.jobs[0].data);
+        const allJobs = res.data.jobs[0].data;
+        setJobs(allJobs);
+      })
+      .catch((err) => {
+        console.log("jobserr",err);
+      });
+  }, [ rerenderJobs]);
+
+  function handleDelete(Jobs_id){
+
+
+    setRerenderJobs(true)
+
+    axios.delete(`http://localhost:8000/api/deleteJob/${Jobs_id}`,{
+      headers: {
+        Authorization: `bearer ${localStorage.getItem("token")}`,
+      },
+    })
+    .then((res)=>{
+      toast.success("Job deleted Successfully")
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+
+  }
+
+  
+
 
   return (
-    
-    <div className='relative mb-48'>
+    <>
+      <ToastContainer />
+      <div className="px-20  w-full">
+        <p className="text-3xl font-medium mb-10">Popular Jobs</p>
+        {getJobs?.map((jobs) => {
+          return (
+            <SingleJob
+              handleDelete={handleDelete}
+              key={jobs._id}
+              
+              jobs={jobs}
+            
+            />
 
-    <div className='myBanner relative z-1' >
-      <img className='h-[45.5vh] w-full object-cover' src="carousel-1.jpg" alt="" />
-    </div>
-    <div className='absolute text-center  top-[50%] -translate-y-[50%]  left-[50%] -translate-x-[50%] z-50'>
-      <h1 className='text-5xl font-bold text-white' >{props.topic}</h1>
-      <section className='flex gap-2 justify-center my-8'>
-        <p className='text-white'>Home  /</p>
-        <p className='text-white'>Pages  /</p>
-        <p className='text-yellow-400'>{props.topic}</p>
-      </section>
-    </div>
-    </div>
-    
-  )
+            // <div
+            //   key={jobs._id}
+            //   className="flex bg-white gap-3 border border-gray-400  rounded-lg p-2 w-full items-center h-fit mb-7 bg-extra "
+            // >
+            //   <img
+            //     className="h-20 rounded-lg"
+            //     src={`${url}${jobs.creatorPic}`}
+            //     alt="oop"
+            //   />
+            //   <div className="mr-auto text-left px-6">
+            //     <p className="font-medium text-xl ">{jobs.title}</p>
+            //     <p>{jobs.type}</p>
+            //     <p className="font-light text-sm"        
+            //     dangerouslySetInnerHTML={{ __html: jobs.description }}>
+
+            //        </p>
+            //   </div>
+            //   <div className="flex items-center gap-3">
+            //     <button
+            //       onClick={() => {
+            //         navigate(`/jobs/${jobs._id}`);
+            //       }}
+            //       className="border border-blue-400 p-5 rounded-xl bg-primary text-white font-bold"
+            //     >
+            //       View Details
+            //     </button>
+            //     {userDetails?.role === "company" ? (
+            //       <>
+            //         <FaRegEdit
+            //           size={25}
+            //           onClick={() => {
+            //             if (userDetails?._id == jobs?.createdBy) {
+            //               // Navigate to the edit page if user is authorized
+            //               navigate(`/jobs/editJob/${jobs._id}`);
+            //             } else {
+            //               // Show an error toast if user is not authorized
+            //               toast.error("Unauthorized", {
+            //                 position: "top-right",
+            //                 autoClose: 1500,
+            //                 hideProgressBar: false,
+            //                 closeOnClick: true,
+            //                 draggable: true,
+            //                 progress: undefined,
+            //                 theme: "dark",
+            //               });
+            //             }
+            //           }}
+            //           className="hover:"
+            //         />
+            //         <MdDelete
+            //          onClick={() => {
+            //           if (jobs.createdBy !== userDetails._id) {
+
+            //             toast.error("Unauthorized", {
+            //               position: "top-right",
+            //               autoClose: 1500,
+            //               hideProgressBar: false,
+            //               closeOnClick: true,
+            //               draggable: true,
+            //               progress: undefined,
+            //               theme: "dark",
+            //             });
+                        
+            //           } else {
+            //             return handleDelete(jobs._id);
+            //           }}}
+            //           size={25}
+            //         />
+            //       </>
+            //     ) : null}
+            //   </div>
+            // </div>
+          );
+        })}
+
+        <button
+          className="border bg-primary p-4 rounded-3xl mb-3"
+          onClick={() => {
+            navigate(`/browse jobs`);
+          }}
+        >
+          Find More Jobs
+        </button>
+        <p className="mb-10 ">Page: 1 of 100</p>
+      </div>
+    </>
+  );
 }
